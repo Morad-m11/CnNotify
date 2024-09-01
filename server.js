e76e788fff19d46bb9f86ffcbaa7ec19fd427331
Mohamed Morad <Mohamed-m11@hotmail.com>
2024-09-01 15:47:28 +0200

    port change

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 import { TARGETS } from './targets.js'
 
 const app = express();
-const port = 3001
+const port = process.env.PORT || 4000;
 app.use(cors())
 
 app.listen(port, () => {
